1bee3cb83fe4ed8d5d918964fb463e731780cf2b
Fyzziihhh <5zziiihhh@gmail.com>
2025-10-06 15:01:59 +0530

    small fixes in repositories

--- src/repositories/wallet/WalletRepository.ts
+++ src/repositories/wallet/WalletRepository.ts
@@ -1,7 +1,9 @@
+import { injectable } from "inversify";
 import { IWallet, Wallet } from "../../models/wallet/Wallet";
 import { BaseRepository } from "../base/BaseRepository";
 import { IWalletRepository } from "./IWalletRepository";
 
+@injectable()
 export class WalletRepository extends BaseRepository<IWallet> implements IWalletRepository {
     constructor() {
         super(Wallet);
