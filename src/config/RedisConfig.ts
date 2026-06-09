f95548ff313afffba2320d235c0fc6af9446b02b
Faizal Raheem <5zziiihhh@gmail.com>
2026-06-09 12:44:51 +0530

    fix(redis): comment out password configuration for Redis connection

--- src/config/RedisConfig.ts
+++ src/config/RedisConfig.ts
@@ -5,7 +5,7 @@ dotenv.config();
 const redis = new Redis({
   host: process.env.REDIS_HOST,
   port: Number(process.env.REDIS_PORT),
-  password: process.env.REDIS_PASSWORD,
+  // password: process.env.REDIS_PASSWORD,
 });
 
 redis.on("connect", () => {
